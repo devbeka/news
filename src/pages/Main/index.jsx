16ced258326949c5db8be199682527ebb02c5e0d
Beknazar <cholponbaevb655@gmail.com>
2023-08-29 23:50:39 +0600

    add pogination, change api

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -4,24 +4,43 @@ import { getNews } from '../../api/apiNews'
 import './styles.scss'
 import NewsList from '../../components/NewsList'
 import Skeleton from '../../components/Skeleton'
+import Pogination from '../../components/Pogination'
 
 const Main = () => {
   const [news, setNews] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [currentPage, setCurrentPage] = useState(1)
+  const totalPages = 10
+  const pageSize = 10
 
-  useEffect(() => {
-    const fetchNews = async () => {
-      try {
-        setIsLoading(true)
-        const response = await getNews()
-        setNews(response.news)
-        setIsLoading(false)
-      } catch (error) {
-        console.log(error)
-      }
+  const fetchNews = async (currentPage) => {
+    try {
+      setIsLoading(true)
+      const response = await getNews(currentPage, pageSize)
+      setNews(response.news)
+      setIsLoading(false)
+    } catch (error) {
+      console.log(error)
     }
-    fetchNews()
-  }, [])
+  }
+  useEffect(() => {
+    fetchNews(currentPage)
+  }, [currentPage])
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1)
+    }
+  }
+  const handlePreviosPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1)
+    }
+  }
+  const handleClickPage = (pageNumber) => {
+    setCurrentPage(pageNumber)
+  }
+
   return (
     <main>
       {news.length > 0 && !isLoading ? (
@@ -34,6 +53,13 @@ const Main = () => {
       ) : (
         <Skeleton type="item" count={10} />
       )}
+      <Pogination
+        handleNextPage={handleNextPage}
+        handlePreviosPage={handlePreviosPage}
+        handleClickPage={handleClickPage}
+        currentPage={currentPage}
+        totalPages={totalPages}
+      />
     </main>
   )
 }
