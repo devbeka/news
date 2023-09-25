754d8127f8bebcf7eca93f4f2808ea9bb4ffe88a
Beknazar <cholponbaevb655@gmail.com>
2023-09-25 18:54:02 +0600

    add latest-news

--- src/components/LatestNews/index.jsx
+++ src/components/LatestNews/index.jsx
@@ -1,10 +1,14 @@
+import { getLatestNews } from '../../api/apiNews'
+import { useFetch } from '../../helpers/hooks/useFetch'
 import BannersList from '../BannersList'
 import './styles.scss'
 
-const LatestNews = ({ banners, isLoading }) => {
+const LatestNews = () => {
+  const { data, isLoading } = useFetch(getLatestNews)
+
   return (
     <section className="latest">
-      <BannersList banners={banners} isLoading={isLoading} />
+      <BannersList banners={data && data?.news} isLoading={isLoading} />
     </section>
   )
 }
